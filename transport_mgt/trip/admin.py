2ca25895def084b3c0a18445cb1a7873a20668f0
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-11-22 19:03:32 +0600

    updated signup,signin,guest form

--- transport_mgt/trip/admin.py
+++ transport_mgt/trip/admin.py
@@ -5,14 +5,14 @@ from .forms import SignUpModelForm, SignInForm
 # Register your models here.
 
 
-class SignUpModelAdmin(admin.ModelAdmin):
-    form = SignUpModelForm
-    # class Meta:
-    #     model = SignUp
+# class SignUpModelAdmin(admin.ModelAdmin):
+#     form = SignUpModelForm
+#     # class Meta:
+#     #     model = SignUp
+#
+#
+# class SignInModelAdmin(admin.ModelAdmin):
+#     form = SignInForm
 
 
-class SignInModelAdmin(admin.ModelAdmin):
-    form = SignInForm
-
-
-admin.site.register(SignUp, SignUpModelAdmin)
+admin.site.register(SignUp)
